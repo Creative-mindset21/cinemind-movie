7ec545a1cfb9d85c7135dcdb933d1350905a2ce0
Creative Mindset <tobiawotoye03@gmail.com>
2025-01-31 16:16:57 +0000

    optimised the search section
    added a 500milliseconds delay when the user stops typing and then display the searched films

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import { createContext } from "react";
 import Movies from "./components/Movies";
 import useFetch from "./customHooks/useFetch";
 import { Analytics } from "@vercel/analytics/react";
+import { useDebounce } from "react-use";
 
-// !CONTEXT PROVIDER API
+// ! CONTEXT PROVIDER API
 export const ValueContext = createContext();
 
+// ! API KEY AND API URL
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_URL = "https://api.themoviedb.org/3";
 
+// ! FETCH OPTIONS
 const options = {
   method: "GET",
   headers: {
@@ -21,11 +24,14 @@ const options = {
 
 function App() {
   const [search, setSearch] = useState("");
-  const url1 = `${API_URL}/search/movie?query=${search}&include_adult=true&language=en-US&page=1`;
+  const [debounce, setDebounce] = useState("");
 
+  useDebounce(() => setDebounce(search), 500, [search]);
+
+  const url1 = `${API_URL}/search/movie?query=${search}&include_adult=true&language=en-US&page=1`;
   const url2 = `${API_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc`;
 
-  const url = search ? url1 : url2;
+  const url = debounce ? url1 : url2;
 
   const { data, loading, errorMessage } = useFetch(url, options);
   let movieData = data.results || [];
